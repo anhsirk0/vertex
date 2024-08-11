606308e8442b552a908ca8aeefdd146e2db6d09c
SolitudeSF <solitudesf@protonmail.com>
2024-08-12 00:04:13 +0300

    Rewrite using xcb

--- src/vertex.nim
+++ src/vertex.nim
@@ -1,16 +1,13 @@
 import
-  os,
-  osproc,
+  std/[os, osproc, strformat],
   parsetoml,
-  strformat,
-  x11/xlib,
-  x11/x
-
+  xcb/xcb
 
 type
-  Prop {.pure.} = enum
+  Prop = enum
     Corner = "corner", Edge = "edge"
-  Location {.pure.} = enum
+
+  Location = enum
     TopLeft = "top_left",
     TopRight = "top_right",
     BottomLeft = "bottom_left",
@@ -20,108 +17,98 @@ type
     LeftBottom = "left_bottom",
     RightBottom = "right_bottom",
 
+  CornerLocation = TopLeft..BottomRight
 
-let configFile = getEnv("HOME") & "/.config/vertex/config.toml"
-if not fileExists(configFile):
-  quit fmt"Config file [{configFile}] does not exists"
+func getCommands[T: enum](toml: TomlValueRef, t: typedesc[T]): array[T, string] =
+  for v in T:
+    result[v] = toml{$v}.getStr
 
-let config = parsetoml.parseFile(configFile)
-echo config
+proc moveCursor(conn: ptr XcbConnection, desX, desY: int16) =
+  discard conn.warpPointer(0.XcbWindow, 0.XcbWindow, 0, 0, 0, 0, desX, desY)
+  discard conn.flush
 
-# config vars
-let  
-  checkInterval = config{"check_interval"}.getInt(200)
-  bounce = config{"bounce"}.getInt(200)
-  edgeOffset = config{"edge_offset"}.getInt(200)
-  cornerReactivation = config{"corner", "reactivation"}.getInt(1000)
-  edgeReactivation = config{"edge", "reactivation"}.getInt(500)  
-
-var
-  display: PDisplay
-  cWindow: Window
-  rWindow: Window
-  rootWindow: Window
-  wX: cint = 0
-  wY: cint = 0
-  rootX: cint = 0
-  rootY: cint = 0
-  mask: cuint = 0
-
-
-proc moveCursor(desX, desY: cint) =
-  discard XWarpPointer(display, cWindow, rootWindow, 0, 0, 0, 0, desX, desY)
-  discard XFlush(display)
-  # discard XSync(display, true.XBool)
-
-
-proc action(prop: Prop, loc: Location, desX: int = 0, desY: int = 0) =
-  let cmd = config{$prop, $loc}.getStr("")
+proc actionCorner(conn: ptr XcbConnection, cmd: string, timeout: int, desX, desY: int16) =
   if cmd != "":
     discard execCmd cmd & " &"
-    if desX != 0 and desY != 0:
-      moveCursor(desX.cint, desY.cint)
-    sleep(if prop == Prop.Corner: cornerReactivation else: edgeReactivation)
+    if desX != 0 or desY != 0:
+      conn.moveCursor(desX, desY)
+    sleep(timeout)
 
+proc actionEdge(conn: ptr XcbConnection, cmd: string, timeout: int) =
+  if cmd != "":
+    discard execCmd cmd & " &"
+    sleep(timeout)
 
 proc init() =
-  display = XOpenDisplay(nil)
-  if display == nil:
-    quit "Failed to open display"
+  let configFile = getConfigDir() / "vertex/config.toml"
+  if not fileExists(configFile):
+    quit fmt"Config file [{configFile}] does not exists"
 
   let
-    screen = XDefaultScreen(display)
-    rootWindow = XRootWindow(display, screen)
-    width = DisplayWidth(display, screen)
-    height = DisplayHeight(display, screen)
+    config = parsetoml.parseFile(configFile)
+
+    # config vars
+    checkInterval = config{"check_interval"}.getInt(200)
+    bounce = config{"bounce"}.getInt(10).int16
+    edgeOffset = config{"edge_offset"}.getInt(10)
+    cornerReactivation = config{"corner", "reactivation"}.getInt(1000)
+    edgeReactivation = config{"edge", "reactivation"}.getInt(500)
+    cornerCommands = config{"corner"}.getCommands(CornerLocation)
+    edgeCommands = config{"edge"}.getCommands(Location)
+
+    conn = xcbConnect(nil, nil)
+    screen = conn.getSetup.rootsIterator.data[0].addr
+    rootWindow = screen.root
+    width = screen.widthInPixels.int16
+    height = screen.heightInPixels.int16
 
   while true:
     sleep checkInterval
-    let p = XQueryPointer(display, rootWindow, cWindow.addr, rWindow.addr,
-                      rootX.addr, rootY.addr, wX.addr, wY.addr, mask.addr)
 
-    if p != 1:
-      quit "Could not read cursor position"
+    let
+      pointerQuery = conn.reply(conn.queryPointer(rootWindow), nil)
+      rootX = pointerQuery.rootX
+      rootY = pointerQuery.rootY
+
+    if pointerQuery.sameScreen == 0: continue
 
     # Corners
     if rootX == 0 and rootY == 0:
-      action(Prop.Corner, Location.TopLeft, bounce, bounce)
+      actionCorner(conn, cornerCommands[TopLeft], cornerReactivation, bounce, bounce)
 
-    if rootX >= width - 1 and rootY == 0:
-      action(Prop.Corner, Location.TopRight, -bounce, bounce)
+    elif rootX >= width - 1 and rootY == 0:
+      actionCorner(conn, cornerCommands[TopRight], cornerReactivation, -bounce, bounce)
 
-    if rootX == 0 and rootY >= height - 1:
-      action(Prop.Corner, Location.BottomLeft, bounce, -bounce)
+    elif rootX == 0 and rootY >= height - 1:
+      actionCorner(conn, cornerCommands[BottomLeft], cornerReactivation, bounce, -bounce)
 
-    if rootX >= width - 1 and rootY >= height - 1:
-      action(Prop.Corner, Location.BottomRight, -bounce, -bounce)
+    elif rootX >= width - 1 and rootY >= height - 1:
+      actionCorner(conn, cornerCommands[BottomRight], cornerReactivation, -bounce, -bounce)
 
     # Edges
-    if rootX > edgeOffset and rootX < width div 2 and rootY == 0:
-      action(Prop.Edge, Location.TopLeft)
+    elif rootX > edgeOffset and rootX < width div 2 and rootY == 0:
+      actionEdge(conn, edgeCommands[TopLeft], edgeReactivation)
 
-    if rootX < width - edgeOffset and rootX > width div 2 and rootY == 0:
-      action(Prop.Edge, Location.TopRight)
+    elif rootX < width - edgeOffset and rootX > width div 2 and rootY == 0:
+      actionEdge(conn, edgeCommands[TopRight], edgeReactivation)
 
-    if rootX > edgeOffset and rootX < width div 2 and rootY >= height - 1:
-      action(Prop.Edge, Location.BottomLeft)
+    elif rootX > edgeOffset and rootX < width div 2 and rootY >= height - 1:
+      actionEdge(conn, edgeCommands[BottomLeft], edgeReactivation)
 
-    if rootX < width - edge_offset and rootX > width div 2 and rootY >= height - 1:
-      action(Prop.Edge, Location.BottomRight)
+    elif rootX < width - edge_offset and rootX > width div 2 and rootY >= height - 1:
+      actionEdge(conn, edgeCommands[BottomRight], edgeReactivation)
 
-    if rootX == 0 and rootY > edgeOffset and rootY < height div 2:
-      action(Prop.Edge, Location.LeftTop)
+    elif rootX == 0 and rootY > edgeOffset and rootY < height div 2:
+      actionEdge(conn, edgeCommands[LeftTop], edgeReactivation)
 
-    if rootX == 0 and rootY < height - edgeOffset and rootY > height div 2:
-      action(Prop.Edge, Location.LeftBottom)
+    elif rootX == 0 and rootY < height - edgeOffset and rootY > height div 2:
+      actionEdge(conn, edgeCommands[LeftBottom], edgeReactivation)
 
-    if rootX == width - 1 and rootY > edgeOffset and rootY < height div 2:
-      action(Prop.Edge, Location.RightTop)
+    elif rootX == width - 1 and rootY > edgeOffset and rootY < height div 2:
+      actionEdge(conn, edgeCommands[RightTop], edgeReactivation)
 
-    if rootX == width - 1 and rootY < height - edge_offset and rootY > height div 2:
-      action(Prop.Edge, Location.RightBottom)
-
+    elif rootX == width - 1 and rootY < height - edge_offset and rootY > height div 2:
+      actionEdge(conn, edgeCommands[RightBottom], edgeReactivation)
 
 when isMainModule:
   init()
-  discard XCloseDisplay(display)
-
